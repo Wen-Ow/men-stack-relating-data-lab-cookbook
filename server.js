6019f27d7ea3c95b9ff2ad941c26694887296e1f
Wen Ow <ow.wen2022@gmail.com>
2025-02-18 20:17:59 -0500

    mvp edits

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const session = require("express-session");
 const isSignedIn = require("./middleware/is-signed-in.js");
 const passUserToView = require("./middleware/pass-user-to-view.js");
 const authController = require("./controllers/auth.js");
-const foodsController = require("./controllers/food.js");
+const foodsController = require("./controllers/foods.js");
 const usersController = require("./controllers/users.js");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
